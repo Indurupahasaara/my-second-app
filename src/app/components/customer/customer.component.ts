6909075fe3698616188d6247d082966be64753f5
Indurupahasara <indurupahasara.sl@gmail.com>
2022-06-17 19:07:26 +0530

    multiple services  add using one import

--- src/app/components/customer/customer.component.ts
+++ src/app/components/customer/customer.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { BehaviorSubject, last, Observable } from 'rxjs';
-import { CustomerService } from 'src/app/services/customer.service';
-
+// import { CustomerService } from 'src/app/services/customer.service';
+import{CustomerService,PostService,ProductService,LoadingService}from'../../services';
 @Component({
   selector: 'app-customer',
   templateUrl: './customer.component.html',
---
+++ src/app/services/index.ts
@@ -0,0 +1,6 @@
+export*from'./customer.service';
+export*from'./loading.service';
+export*from'./post.service';
+export*from'./product.service';
+
+//use for  collect all servises this all servises call to customer component.ts using one import elemnet( in 6th line)
